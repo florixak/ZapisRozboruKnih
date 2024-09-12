d5c663f809602a91254e5583e32826c3e29949f0
florixak <florixakcz@seznam.cz>
2024-09-12 18:47:51 +0200

    Update

--- zapis-rozbor-knih/src/components/Home.jsx
+++ zapis-rozbor-knih/src/components/Home.jsx
@@ -4,14 +4,14 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-10">
       <h1 className="text-3xl font-bold">Vyber si co chceš procvičit:</h1>
-      <div className="flex flex-row gap-10 text-lg">
-        <Link className="border-2 p-2" to={"/smery"}>
+      <div className="flex flex-row gap-10 px-32 py-16 text-lg">
+        <Link className="border-2 p-2 hover:bg-slate-900" to={"/smery"}>
           Umělecké směry a období
         </Link>
-        <Link className="border-2 p-2" to={"/authors"}>
+        <Link className="border-2 p-2 hover:bg-slate-900" to={"/authors"}>
           Autoři
         </Link>
-        <Link className="border-2 p-2" to={"/books"}>
+        <Link className="border-2 p-2 hover:bg-slate-900" to={"/books"}>
           Knížky
         </Link>
       </div>
